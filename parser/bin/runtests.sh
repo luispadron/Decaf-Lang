52d6cf491e7e8e4b780cf7f1e9839f3f6c718ed2
Luis <heyluispadron@gmail.com>
2019-02-01 14:45:35 -0500

    update test script

--- parser/bin/runtests.sh
+++ parser/bin/runtests.sh
@@ -11,6 +11,8 @@ printf 'samples\nsample_tests/ours\ndcc' | ./bin/gen_tests.sh > /dev/null
 
 set -e
 ERROR=false
+PASSED=0
+TOTAL=0
 
 for file in sample_tests/correct/*
 do  
@@ -19,16 +21,22 @@ do
     {
         diff $file sample_tests/ours/$filename.txt > sample_tests/diffs/$filename.diff    
     } || {
-        echo "test failed for file: $filename❗️"
-        echo "check diff here: sample_tests/diffs/$filename.diff"
-        echo "--------------------------------------------------"
+        echo "test failed for file: $filename ❗️"
         ERROR=true
     }
+
+    if $ERROR;
+    then
+        echo "check diff: sample_tests/diffs/$filename.diff"
+        echo "--------------------------------------------------"
+    else
+        PASSED=$((PASSED+1))
+        echo "test passed for file: $filename ✅"
+        echo "--------------------------------------------------"
+    fi
+
+    ERROR=false
+    TOTAL=$((TOTAL+1))
 done
 
-if $ERROR;
-then
-    echo "tests failed❗️"
-else 
-    echo "tests succeeded✅"
-fi
+echo "passed $PASSED / $TOTAL tests"
