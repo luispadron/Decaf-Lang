74919ea19e37a1b178f65cc5295dc23be9b171b3
Luis <heyluispadron@gmail.com>
2019-03-02 15:53:52 -0500

    remove cached ignored files

--- semantic-analyzer/cmake-build-debug/CMakeFiles/analyzer.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/analyzer"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
