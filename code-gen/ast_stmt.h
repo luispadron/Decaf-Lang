31081a55b7c69a79a5bd544db68c69f46e44b070
Luis <heyluispadron@gmail.com>
2019-03-14 15:07:44 -0400

    style changes

--- code-gen/ast_stmt.h
+++ code-gen/ast_stmt.h
@@ -3,7 +3,7 @@
  * The Stmt class and its subclasses are used to represent
  * statements in the parse tree.  For each statment in the
  * language (for, if, return, etc.) there is a corresponding
- * node class for that construct. 
+ * node class for that construct.
  *
  * pp4: You will need to extend the Stmt classes to implement
  * code generation for statements.
@@ -19,14 +19,14 @@
 class Decl;
 class VarDecl;
 class Expr;
-  
+
 class Program : public Node
 {
   protected:
      List<Decl*> *decls;
-     
+
   public:
-     Program(List<Decl*> *declList);
+     explicit Program(List<Decl*> *declList);
      void Check();
      void Emit();
 };
@@ -35,83 +35,78 @@ class Stmt : public Node
 {
   public:
      Stmt() : Node() {}
-     Stmt(yyltype loc) : Node(loc) {}
+     explicit Stmt(yyltype loc) : Node(loc) {}
 };
 
-class StmtBlock : public Stmt 
-{
-  protected:
+class StmtBlock : public Stmt {
+protected:
     List<VarDecl*> *decls;
     List<Stmt*> *stmts;
-    
-  public:
+
+public:
     StmtBlock(List<VarDecl*> *variableDeclarations, List<Stmt*> *statements);
 };
 
-  
+
 class ConditionalStmt : public Stmt
 {
   protected:
     Expr *test;
     Stmt *body;
-  
+
   public:
     ConditionalStmt(Expr *testExpr, Stmt *body);
 };
 
-class LoopStmt : public ConditionalStmt 
+class LoopStmt : public ConditionalStmt
 {
   public:
     LoopStmt(Expr *testExpr, Stmt *body)
             : ConditionalStmt(testExpr, body) {}
 };
 
-class ForStmt : public LoopStmt 
+class ForStmt : public LoopStmt
 {
   protected:
     Expr *init, *step;
-  
+
   public:
     ForStmt(Expr *init, Expr *test, Expr *step, Stmt *body);
 };
 
-class WhileStmt : public LoopStmt 
+class WhileStmt : public LoopStmt
 {
   public:
     WhileStmt(Expr *test, Stmt *body) : LoopStmt(test, body) {}
 };
 
-class IfStmt : public ConditionalStmt 
-{
-  protected:
+class IfStmt : public ConditionalStmt {
+protected:
     Stmt *elseBody;
-  
-  public:
+
+public:
     IfStmt(Expr *test, Stmt *thenBody, Stmt *elseBody);
 };
 
-class BreakStmt : public Stmt 
-{
-  public:
-    BreakStmt(yyltype loc) : Stmt(loc) {}
+class BreakStmt : public Stmt {
+public:
+    explicit BreakStmt(yyltype loc) : Stmt(loc) {}
 };
 
-class ReturnStmt : public Stmt  
-{
-  protected:
+class ReturnStmt : public Stmt {
+protected:
     Expr *expr;
-  
-  public:
+
+public:
     ReturnStmt(yyltype loc, Expr *expr);
 };
 
-class PrintStmt : public Stmt
-{
-  protected:
+class PrintStmt : public Stmt {
+protected:
     List<Expr*> *args;
-    
-  public:
-    PrintStmt(List<Expr*> *arguments);
+
+public:
+    explicit PrintStmt(List<Expr*> *arguments);
 };
 
 
